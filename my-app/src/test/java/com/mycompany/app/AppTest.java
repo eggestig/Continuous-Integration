f03f2e78c9d9e2babeeaf791290e3d9f4d63cf5c
Robin Eggestig <eggestig@kth.se>
2024-02-06 11:16:39 +0100

    feat/#3 - Implement smallest-java-ci
    Implements smallest-java-ci and adds a connections test

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,7 +1,13 @@
 package com.mycompany.app;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.server.Server;
 import org.junit.Test;
 
 /**
@@ -16,5 +22,25 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    /**
+     * Test connection to server
+     */
+    @Test
+    public void testConnection() throws Exception
+    {
+        // Start server
+        Server server = new Server(8080);
+        server.setHandler(new App()); 
+        server.start();
+
+        // Test connection
+        HttpURLConnection http = (HttpURLConnection)new URL("http://localhost:8080/").openConnection();
+        http.connect();
+        assertTrue("Response Code", http.getResponseCode() == HttpStatus.OK_200);
+
+        // Stop server
+        server.stop();
+        server.join();
     }
 }
