a5e4708b512a0eb6dd5469f9d4a22bbb85d832fb
Henrik Åkesson <hakes@kth.se>
2024-02-08 14:26:40 +0100

    Feat/#14 Notification
    Created a setCommitStatus method which sets the commit status on the repository with REST API.
    Made a .env file to store auth token for REST API authentication.

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -2,32 +2,36 @@ package com.mycompany.app;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.srujankujmar.commons.config.SetupConfig;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
- 
+
 import java.io.IOException;
 import java.util.stream.Collectors;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.kohsuke.github.GHCommitState;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
-/** 
- Skeleton of a ContinuousIntegrationServer which acts as webhook
- See the Jetty documentation for API documentation of those classes.
-*/
-public class App extends AbstractHandler
-{
+/**
+ * Skeleton of a ContinuousIntegrationServer which acts as webhook
+ * See the Jetty documentation for API documentation of those classes.
+ */
+public class App extends AbstractHandler {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response) 
-        throws IOException, ServletException
-    {
+            Request baseRequest,
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
@@ -48,7 +52,8 @@ public class App extends AbstractHandler
             System.out.println("Received GitHub push event for repository: " + repositoryName +
                     ", pushed to branch: " + pushedBranch);
 
-            //System.out.println("Received GitHub push event: " + payload);
+            // System.out.println("Received GitHub push event: " + payload);
+            setCommitStatus(jsonNode);
         }
 
         // Respond with a 200 OK status
@@ -58,15 +63,55 @@ public class App extends AbstractHandler
         // for example
         // 1st clone your repository
         // 2nd compile the code with mvn
+
+
         response.getWriter().println("CI job done");
+
     }
- 
+
+    private static GitHub getGithub(final String token) {
+        try {
+            return GitHub.connectUsingOAuth("https://api.github.com", token);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setCommitStatus(JsonNode payload) {
+        try {
+            // Make sure to run before: "export AUTH_TOKEN="<insert_tok>""
+            String token = System.getenv("AUTH_TOKEN");
+            GitHub github = getGithub(token);
+
+            String owner = payload.path("repository")
+                            .path("owner")
+                            .path("name")
+                            .asText();
+            String repoName = payload.path("repository")
+                            .path("name")
+                            .asText();
+
+            String sha1 = payload.path("after").asText();
+
+            String description = "The build succeeded!";
+
+            String targetUrl = payload.path("head_commit").path("url").asText();
+
+            GHRepository repository = github.getRepository(owner + "/" + repoName);
+            repository.createCommitStatus(sha1, GHCommitState.SUCCESS, targetUrl, description);
+
+            System.out.println("Commit status updated successfully.");
+        } catch (Exception e) {
+            System.out.println("Commit status update failed.");
+            e.printStackTrace();
+        }
+    }
+
     // used to start the CI server in command line
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
         Server server = new Server(8080);
-        server.setHandler(new App()); 
+        server.setHandler(new App());
         server.start();
         server.join();
     }
---
+++ my-app/trial.sh
@@ -0,0 +1,1 @@
+export AUTH_TOKEN "test token"export AUTH_TOKEN "test token"
