de143ffe388c0c0b94740f7a6b293b0faa61365a
Anton Sederlin <anton.sederlin@gmail.com>
2024-02-08 12:45:55 +0100

    feat/#13 - Implemented the projectBuilder method
    This method will automatically run mvn package in the directory of the cloned branch containing the most recent commit

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,14 +6,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
- 
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-
 /** 
  Skeleton of a ContinuousIntegrationServer which acts as webhook
  See the Jetty documentation for API documentation of those classes.
@@ -52,13 +54,60 @@ public class App extends AbstractHandler
         }
 
         // Respond with a 200 OK status
-        response.setStatus(HttpServletResponse.SC_OK);
-
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
         // 2nd compile the code with mvn
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        projectBuilder(System.getProperty("user.dir"));
+
         response.getWriter().println("CI job done");
+    }
+
+    public static String projectBuilder(String path){
+       
+        String buildResult = "";
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(new String[]{"mvn", "package"}); 
+            processBuilder.directory(new java.io.File(path));
+            Process commandRunner = processBuilder.start();
+
+            String output = captureOutput(commandRunner.getInputStream());
+            int exitCode = commandRunner.waitFor();
+
+            System.out.println("Captured Output:\n" + output);
+            System.out.println("Exit Code: " + exitCode);
+
+            if (output.contains("BUILD SUCCESS")) {
+                buildResult = "SUCCESS";
+
+            } else if (output.contains("BUILD FAILURE")){
+                buildResult = "FAILURE";
+            }
+
+        } catch (IOException | InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println(buildResult);
+        return buildResult;
+
+    }
+
+    public static String captureOutput(InputStream inputStream) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            StringBuilder output = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append(System.lineSeparator());
+            }
+
+            return output.toString();
+        }
     }
  
     // used to start the CI server in command line
--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,14 +1,20 @@
 package com.mycompany.app;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.server.Server;
-import org.junit.Test;
+import org.junit.Test; 
+
 
 /**
  * Unit test for simple App.
@@ -42,5 +48,24 @@ public class AppTest
         // Stop server
         server.stop();
         server.join();
+    }
+
+    /**
+     * Test that the helper method captureOutput
+     * captures the correct console output for a given input.
+     */
+    @Test
+    public void testCaptureOutput() throws IOException {
+
+        String input = "[INFO] Scanning for projects...\n[INFO] BUILD FAILURE\n[INFO] Total time:  0.033 s";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+
+        String output = App.captureOutput(inputStream);
+
+        String expectedOutput = "[INFO] Scanning for projects..." + System.lineSeparator() +
+                                "[INFO] BUILD FAILURE" + System.lineSeparator() +
+                                "[INFO] Total time:  0.033 s" + System.lineSeparator();
+
+        assertEquals(expectedOutput, output);
     }
 }
